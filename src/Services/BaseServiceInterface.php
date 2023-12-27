2d62b01581fc6350c27beb962454d87d54d58a13
Ivan Sotelo <isotelo@controlla.com.mx>
2023-12-27 15:08:42 -0600

    feat: add base classes

---
+++ src/Services/BaseServiceInterface.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Controlla\Core\Services;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseServiceInterface
+{
+    /**
+     * Find models by attributes
+     * @param mixed $id
+     * @return Model|null
+     */
+    public function find($id);
+
+    /**
+     * Find or fail the model
+     * @param mixed $id
+     * @return mixed
+     */
+    public function findOrFail($id);
+
+    /**
+     * Return all items
+     * @return Collection|null
+     */
+    public function all();
+
+    /**
+     * Create model
+     * @param array|mixed $data
+     * @return Model|null
+     */
+    public function create($data);
+
+    /**
+     * Update a model
+     * @param int|mixed $id
+     * @param array|mixed $data
+     * @return bool|mixed
+     */
+    public function update($id, array $data);
+
+    /**
+     * Delete a model
+     * @param int|Model $id
+     */
+    public function delete($id);
+
+    /**
+     * multiple delete
+     * @param array $id
+     * @return mixed
+     */
+    public function destroy(array $id);
+}
